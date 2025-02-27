9785289f56c54da3f34768ac5077123fa4f39ae5
robinyeon <robin.yeon@gmail.com>
2025-02-27 11:16:17 +0900

    feat: closing popup, input keyword

--- tests/searchTest.spec.js
+++ tests/searchTest.spec.js
@@ -1,7 +1,20 @@
-import {test, expect} from "@playwright/test"
+import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-    await page.goto("https://www.nl.go.kr/");
-  
-    await expect(page).toHaveTitle(/국립/);
-  });
+test.beforeEach(async ({ page }) => {
+  console.log(`Running ${test.info().title}`);
+  await page.goto('https://www.nl.go.kr/');
+});
+
+test('팝업 닫고 검색어 입력', async ({ page }) => {
+  const popupCloseButtons = page.locator("a[onclick*='fn_popupClose']");
+  const count = await popupCloseButtons.count();
+
+  if (count > 0) {
+    await popupCloseButtons.first().click();
+    console.log('팝업 닫기');
+  }
+
+  const searchInput = page.locator('#main_input-text1, #main_input-text2'); // 반응형 대응
+  await searchInput.first().fill('호빗');
+  console.log("검색어 '호빗' 입력");
+});
